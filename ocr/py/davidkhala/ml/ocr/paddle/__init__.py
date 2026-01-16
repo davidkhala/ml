64b7efaca2e6f24134642931372bd1bbff181335
David Liu <david-khala@hotmail.com>
2026-01-16 16:37:17 +0800

    wip: paddleNLP on ubuntu

--- ocr/py/davidkhala/ml/ocr/paddle/__init__.py
+++ ocr/py/davidkhala/ml/ocr/paddle/__init__.py
@@ -6,17 +6,15 @@ from davidkhala.utils.syntax.path import home_resolve, delete
 
 class Client:
 
-    def init(self, type: Literal['all', 'focus']):
-
+    def init(self):
         from paddleocr import PaddleOCR
         self.all = PaddleOCR(use_doc_unwarping=True)
-
     @staticmethod
     def clean():
         """clean up downloaded models on local disk"""
         delete(home_resolve('.paddlex', 'official_models'))
 
-    def process(self, file: Path) -> list[dict] | dict:
+    def process(self, file: Path) -> list[str]:
         results = self.all.predict(str(file))
         assert len(results) == 1
         return results[0]['rec_texts']
--- ocr/py/tests/paddle_test.py
+++ ocr/py/tests/paddle_test.py
@@ -1,21 +1,43 @@
+import os
 import unittest
 from pathlib import Path
+from unittest import skipIf
 
-from davidkhala.ml.ocr.paddle import Client
 
-
-class SDKTestCase(unittest.TestCase):
+class PaddleOCRTestCase(unittest.TestCase):
     def setUp(self):
+        from davidkhala.ml.ocr.paddle import Client
         self.client = Client()
 
+    def test_all_text(self):
+        file = Path(__file__).parent / "fixtures" / "transcript.png"
+        self.client.init()
+        tokens = self.client.process(file)
+        self.assertIn('High School Transcript', tokens)
 
+    @skipIf(os.environ.get("CI") is not None, "used for cleanup local only")
     def test_clean(self):
         self.client.clean()
 
-    def test_schema(self):
-        file = Path(__file__).parent / "fixtures" / "transcript.png"
-        self.client.init('focus')
-        self.client.process(file)
+
+class PaddleNLPTestCase(unittest.TestCase):
+    def test_raw_sample(self):
+        from pprint import pprint
+        from paddlenlp import Taskflow
+
+        schema = ['时间', '选手', '赛事名称']  # Define the schema for entity extraction
+        ie = Taskflow('information_extraction',
+                      schema=schema,
+                      schema_lang="zh",
+                      batch_size=1,
+                      model='paddlenlp/PP-UIE-0.5B',
+                      precision='float16')
+        pprint(
+            ie("2月8日上午北京冬奥会自由式滑雪女子大跳台决赛中中国选手谷爱凌以188.25分获得金牌！"))  # Better print results using pprint
+        # 输出
+        # [{'时间': [{'text': '2月8日上午'}],
+        #   '赛事名称': [{'text': '北京冬奥会自由式滑雪女子大跳台决赛'}],
+        #   '选手': [{'text': '谷爱凌'}]}]
 
 
 if __name__ == '__main__':
